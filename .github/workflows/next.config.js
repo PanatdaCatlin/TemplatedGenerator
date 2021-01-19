d0596fac4e1170e83ab91062ce54d17b523a8bfc
Panatda Inthala <panatdainthala@gmail.com>
2021-01-18 21:32:37 -0800

    simplified config

--- .github/workflows/next.config.js
+++ .github/workflows/next.config.js
@@ -1,21 +1,3 @@
-const webpack = require('webpack')
-
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-
-const assetPrefix = isProd ? '/TemplatedGenerator' : ''
-
 module.exports = {
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: assetPrefix,
-  webpack: config => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      }),
-    )
-
-    return config
-  },
-}
+  assetPrefix: "/TemplatedGenerator",
+};
