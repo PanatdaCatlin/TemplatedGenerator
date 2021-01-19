0e7c69887eeec22e62a91e97f06348b09501c2f4
PanatdaCatlin <panatdacatlin@gmail.com>
2021-01-19 06:06:31 +0000

    Deploying to gh-pages from  @ f593a108076eed089529fe89699b0717b7093b8f 🚀

---
+++ _next/static/I4c2dbPjtS8NEf_RbGI0D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fdac0238b268cce95963c.css","static\u002Fchunks\u002Fpages\u002Findex-c35a1477444f942f33b2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b2e8d81a7624e665d650.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
