dba05757c3626dc4a5672f5787ad6d29dc80827e
Eric Catlin <eroccatlun@gmail.com>
2021-10-30 18:28:28 -0700

    More responsive design & Guided Tour added

--- components/sql-workflow/CityIdTable.js
+++ components/sql-workflow/CityIdTable.js
@@ -1,60 +1,50 @@
-
 import React, { useEffect, useReducer, useState } from "react";
 
 import H2 from "../H2";
 import ReactDataSheet from "react-datasheet";
-import 'react-datasheet/lib/react-datasheet.css';
+import "react-datasheet/lib/react-datasheet.css";
 
-const tableHeaders = [{ readOnly: true, value: "City", width: 100 }, { readOnly: true, value: 'Id', width: 100 }];
-const defaultData = [[{ value: "Seattle" }, { value: 101 }]];
+const CityIdTable = ({ tableData, setTableData }) => {
 
-const CityIdTable = ({ getTableData }) => {
-    const [tableData, setTableData] = useState([tableHeaders, ...defaultData]);
-
-    const updateTableData = (newData) => {
-        // Chop off header before updating parent
-        const [, ...justData] = newData
-        getTableData?.(justData);
-        setTableData(newData);
-    }
-
-    useEffect(() => {
-        // Chop off header before updating parent
-        const [, ...justData] = tableData
-        getTableData?.([, ...justData]);
-    }, [])
-    return <div
-        className="column padded elevated rounded bordered flex-start"
-        style={{ flexGrow: 1 }}
+  return (
+    <div
+      className="column padded elevated rounded bordered flex-start"
+      style={{ flexGrow: 1 }}
     >
-        <H2 text={"Cities & Ids"}></H2>
-        <div className="row flex-wrap">
-            <div className="column keymap-list padded flex-start">
-                <div className=" white flex-start ">
-                    <ReactDataSheet data={tableData} valueRenderer={({ value }) => value}
-                        onCellsChanged={(changes, additions) => {
-                            const grid = tableData;
-                            changes.forEach(({ cell, row, col, value }) => {
-                                grid[row][col] = { ...grid[row][col], value };
-                            });
-                            additions?.forEach(({ row, col, value }) => {
-                                if (!grid[row]) {
-                                    grid.push([]);
-                                }
-                                if (!grid[row][col]) {
-                                    grid[row].push([])
-                                }
-                                grid[row][col] = { value }
-                            })
+      <H2 text={"Cities & Ids"}></H2>
+      <div className="row flex-wrap">
+        <div className="column keymap-list padded flex-start">
+          <div className=" white flex-start data-table">
+            <ReactDataSheet
+              data={tableData}
+              valueRenderer={({ value }) => value}
+              onCellsChanged={(changes, additions) => {
+                const grid = tableData;
+                changes.forEach(({ cell, row, col, value }) => {
+                  grid[row][col] = { ...grid[row][col], value };
+                });
+                additions?.forEach(({ row, col, value }) => {
+                  if (!grid[row]) {
+                    grid.push([]);
+                  }
+                  if (!grid[row][col]) {
+                    grid[row].push([]);
+                  }
+                  grid[row][col] = { value };
+                });
 
+                setTableData(grid);
+              }}
+            ></ReactDataSheet>
 
-                            updateTableData(grid);
-                        }}></ReactDataSheet>
-
-                    <button onClick={() => updateTableData([...tableData, [{}, {}]])} >Add Row</button>
-                </div>
-            </div></div>
+            <button onClick={() => setTableData([...tableData, [{}, {}]])}>
+              Add Row
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default CityIdTable;
--- components/sql-workflow/RenderOutputs.js
+++ components/sql-workflow/RenderOutputs.js
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import H2 from "../H2";
 
-
-const RenderOutputs = ({ data, id, city, template }) => {
-    const [outputs, setOutputs] = useState([]);
-    return <div
-        className="column padded elevated rounded bordered flex-start"
-        style={{ flexGrow: 1 }}
+const RenderOutputs = ({ tableData, id, city, template }) => {
+  const [outputs, setOutputs] = useState(null);
+  const justData = useMemo(() => {
+    const [, ...rest] = tableData;
+    return rest;
+  }, [tableData]);
+  return (
+    <div
+      className="column padded elevated rounded bordered flex-start"
+      style={{ flexGrow: 1 }}
     >
-        <H2 text={"Render Outputs"}></H2>
-        <button onClick={() =>
-            setOutputs(data.map(([{ value: cityVal }, { value: idVal }]) =>
-                template.replaceAll(city, cityVal).replaceAll(id, idVal))
-            )} >Generate</button>
-        <div className="column padded">
-            {outputs.map((output, i) => <div className="row" key={i}>{output}</div>)}
-        </div>
+      <button
+        className="generate-outputs row elevated rounded padded"
+        style={{ width: "100px" }}
+        onClick={() =>
+          setOutputs(
+            justData.map(([{ value: cityVal }, { value: idVal }]) =>
+              template.replaceAll(city, cityVal).replaceAll(id, idVal)
+            )
+          )
+        }
+      >
+        Generate
+      </button>
 
-    </div >
-}
+      {outputs && (
+        <>
+          <H2 text={"Rendered Outputs"}></H2>
+
+          <div className="column padded">
+            {outputs.map((output, i) => (
+              <div className="row" key={i}>
+                {output}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default RenderOutputs;
